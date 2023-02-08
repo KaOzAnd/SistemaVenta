e466e05e98561532703ae106c98bfd4b13b11393
Andres Jurgensen Álzate <andresjgsalzate@gmail.com>
2023-02-07 20:16:51 -0500

    Clase numero 3

--- SistemaVenta.DAL/Repositorios/VentaRepository.cs
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -45,9 +45,50 @@ namespace SistemaVenta.DAL.Repositorios
             throw new NotImplementedException();
         }
 
-        public Task<Venta> Registrar(Venta modelo)
+        public async Task<Venta> Registrar(Venta modelo)
         {
-            throw new NotImplementedException();
+            Venta ventaGenerada = new Venta();
+
+            using(var transaction = _dbcontext.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach(DetalleVenta dv in modelo.DetalleVenta)
+                    {
+                        Producto producto_encontrado = _dbcontext.Productos.Where(p => p.IdProducto == dv.IdProducto).First();
+                        producto_encontrado.Stock = producto_encontrado.Stock - dv.Cantidad;
+                        _dbcontext.Productos.Update(producto_encontrado);
+                    }
+                    await _dbcontext.SaveChangesAsync();
+
+                    NumeroDocumento correlativo = _dbcontext.NumeroDocumentos.First();
+                    correlativo.UltimoNumero = correlativo.UltimoNumero + 1;    
+                    correlativo.FechaRegistro = DateTime.Now;
+
+                    _dbcontext.NumeroDocumentos.Update(correlativo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    int CantidadDigitos = 4;
+                    string ceros = string.Concat(Enumerable.Repeat("0", CantidadDigitos));
+                    string numeroVenta = ceros + correlativo.UltimoNumero.ToString();
+
+                    numeroVenta = numeroVenta.Substring(numeroVenta.Length - CantidadDigitos, CantidadDigitos);
+
+                    modelo.NumeroDocumento = numeroVenta;
+                    await _dbcontext.Venta.AddAsync(modelo);
+                    await _dbcontext.SaveChangesAsync();
+
+                    ventaGenerada = modelo;
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+
+                return ventaGenerada;
+            }
         }
     }
 }
