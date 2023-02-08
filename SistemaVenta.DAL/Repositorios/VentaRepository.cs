6182d1b0daae4922178adcaccc4171df5e9ca9de
Andres Jurgensen Álzate <andresjgsalzate@gmail.com>
2023-02-07 19:52:41 -0500

    APISistemaVenta

---
+++ SistemaVenta.DAL/Repositorios/VentaRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.DBContext;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class VentaRepository :IGenericRepository<Venta>, IVentaRepository
+    {
+        private readonly DbventaContext _dbcontext;
+
+        public VentaRepository(DbventaContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public Task<IQueryable<Venta>> Consultar(Expression<Func<Venta, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venta> Crear(Venta modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(Venta modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(Venta modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venta> Obtener(Expression<Func<Venta, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Venta> Registrar(Venta modelo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using SistemaVenta.DAL.DBContext;
 using System;
 using System.Collections.Generic;
@@ -13,11 +12,11 @@ namespace SistemaVenta.IOC
 {
     public static class Dependencia
     {
-        public static void InyectarDependecias(this IServiceCollection services, IConfiguration configuration)
+        public static void InyectarDependencias(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<DbventaContext>(Options =>
+            services.AddDbContext<DbventaContext>(options =>
             {
-                Options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSQL"));
             });
         }
     }
